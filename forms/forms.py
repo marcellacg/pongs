46bed83572d2971f904143912d00c46744367eea
marcellacg <marcelladacosta@gmail.com>
2023-05-09 22:45:47 -0300

    att pet

--- forms/forms.py
+++ forms/forms.py
@@ -22,9 +22,24 @@ class FormularioLogin(FlaskForm):
     
 
 class FormularioPet(FlaskForm):
-    nomePet = StringField('nome', validators =[DataRequired()])
-    idade = StringField('idade', validators =[DataRequired()])
-    especie = StringField('especie', validators =[DataRequired()])
-    observacoes = StringField('observacoes', validators =[DataRequired()])
+    nomePet = StringField('nome',
+        [
+        validators.Regexp('[a-zA-Z]+', message="Nome deve conter somente letras"),
+        validators.Length(min=2, max=25, message="Nome deve ter entre 5 e 25 characteres")
+        ])
+    idade = StringField('idade',
+        [
+        validators.Regexp('^[0-9]*$', message="idade deve conter somente números"),
+        validators.Length(min=1, max=3, message="idade deve ter no máximo 3 dígitos")
+        ])
+    especie = StringField('especie',
+        [
+        validators.Regexp('[a-zA-Z]+', message="Espécie deve conter somente letras")
+        ])
+    observacoes = StringField('observacoes',
+        [
+        validators.Regexp('^[a-zA-Z0-9_.-]*$', message=""),
+        validators.Length(min=0, max=500, message="As observações devem ter no máximo 500 palavras")
+        ])
     user_id = IntegerField()
     submit = SubmitField('cadastrar')
