8d95ffcd72cd86649e28e7a07211a31217c18090
Rhavy Maia Guedes <rmg3@cin.ufpe.br>
2023-05-10 20:26:31 -0300

    Update pet.py
    Adicionar construtor e relacionamento com User na classe Pets.

--- models/pet.py
+++ models/pet.py
@@ -8,8 +8,18 @@ class Pets(UserMixin, db.Model):
   id = db.Column(db.Integer, primary_key=True)
   nome = db.Column(db.String(50))
   idade = db.Column(db.String(4))
-  especie = db.Column(db.String())
-  observacoes = db.Column(db.String(500))
+  especie = db.Column(db.String()) #TODO: Criar a entidade Especie
+  observacoes = db.Column(db.String(500)) #TODO: transformar num relacionamento 1..n
   
   user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+  
+  user = db.relationship("User", uselist=False)
+  
+  def __init__(self, nome, idade, especie, observacoes, user):
+    self.nome = nome
+    self.idade = idade
+    self.especie = especie
+    self.observacoes = observacoes
+    self.user = user
+    
 
